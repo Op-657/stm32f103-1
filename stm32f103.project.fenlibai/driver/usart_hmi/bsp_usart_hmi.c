c485031704a9658c968888cffdca250efe994f8c
Opt-657 <1610408962@qq.com>
2017-07-26 12:04:37 +0800

    feature : wind pendulun version 1

---
+++ stm32f103.project.fenlibai/driver/usart_hmi/bsp_usart_hmi.c
@@ -0,0 +1,80 @@
+#include "bsp_usart_hmi.h"
+
+ /**
+  * @brief  配置嵌套向量中断控制器NVIC
+  * @param  无
+  * @retval 无
+  */
+static void NVIC_Configuration(void)
+{
+  NVIC_InitTypeDef NVIC_InitStructure;
+  /* 配置USART为中断源 */
+  NVIC_InitStructure.NVIC_IRQChannel = HMI_USART_IRQ;
+  /* 抢断优先级*/
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+  /* 子优先级 */
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+  /* 使能中断 */
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  /* 初始化配置NVIC */
+  NVIC_Init(&NVIC_InitStructure);
+}
+
+ /**
+  * @brief  USART GPIO 配置,工作参数配置
+  * @param  无
+  * @retval 无
+  */
+void USART3_Config(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+
+	// 打开串口GPIO的时钟
+	HMI_USART_GPIO_APBxClkCmd(HMI_USART_GPIO_CLK, ENABLE);
+	
+	// 打开串口外设的时钟
+	HMI_USART_APBxClkCmd(HMI_USART_CLK, ENABLE);
+
+	// 将USART Tx的GPIO配置为推挽复用模式
+	GPIO_InitStructure.GPIO_Pin = HMI_USART_TX_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(HMI_USART_TX_GPIO_PORT, &GPIO_InitStructure);
+
+  // 将USART Rx的GPIO配置为浮空输入模式
+	GPIO_InitStructure.GPIO_Pin = HMI_USART_RX_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(HMI_USART_RX_GPIO_PORT, &GPIO_InitStructure);
+	
+	// 配置串口的工作参数
+	// 配置波特率
+	USART_InitStructure.USART_BaudRate = HMI_USART_BAUDRATE;
+	// 配置 针数据字长
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	// 配置停止位
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	// 配置校验位
+	USART_InitStructure.USART_Parity = USART_Parity_No ;
+	// 配置硬件流控制
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	// 配置工作模式，收发一起
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	// 完成串口的初始化配置
+	USART_Init(HMI_USARTx, &USART_InitStructure);
+	
+	// 串口中断优先级配置
+	NVIC_Configuration();
+	
+	// 使能串口接收中断
+	USART_ITConfig(HMI_USARTx, USART_IT_RXNE, ENABLE);	
+	
+	// 使能串口
+	USART_Cmd(HMI_USARTx, ENABLE);		
+
+  // 清除发送完成标志
+	//USART_ClearFlag(USART1, USART_FLAG_TC);     
+}
+
+
+
