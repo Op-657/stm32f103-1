fa06b95eda6cd7a1b9a3d32628ff9861340ae6fa
Opt-657 <1610408962@qq.com>
2017-09-08 11:11:49 +0800

    updata : Infrared remote updata

--- stm32f103ve.tim.Infrared_remote/driver/GeneralTim/bsp_GeneralTim.c
+++ stm32f103ve.tim.Infrared_remote/driver/GeneralTim/bsp_GeneralTim.c
@@ -6,34 +6,13 @@ static void GENERAL_TIM_GPIO_Config(void)
   GPIO_InitTypeDef GPIO_InitStructure;
 
   // 输出比较通道1 GPIO 初始化
-	RCC_APB2PeriphClockCmd(GENERAL_TIM_CH1_GPIO_CLK, ENABLE);
+  RCC_APB2PeriphClockCmd(GENERAL_TIM_CH1_GPIO_CLK, ENABLE);
   GPIO_InitStructure.GPIO_Pin =  GENERAL_TIM_CH1_PIN;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GENERAL_TIM_CH1_PORT, &GPIO_InitStructure);
 	
-	// 输出比较通道2 GPIO 初始化
-	RCC_APB2PeriphClockCmd(GENERAL_TIM_CH2_GPIO_CLK, ENABLE);
-  GPIO_InitStructure.GPIO_Pin =  GENERAL_TIM_CH2_PIN;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GENERAL_TIM_CH2_PORT, &GPIO_InitStructure);
-	
-	// 输出比较通道3 GPIO 初始化
-	RCC_APB2PeriphClockCmd(GENERAL_TIM_CH3_GPIO_CLK, ENABLE);
-  GPIO_InitStructure.GPIO_Pin =  GENERAL_TIM_CH3_PIN;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GENERAL_TIM_CH3_PORT, &GPIO_InitStructure);
-	
-	// 输出比较通道4 GPIO 初始化
-	RCC_APB2PeriphClockCmd(GENERAL_TIM_CH3_GPIO_CLK, ENABLE);
-  GPIO_InitStructure.GPIO_Pin =  GENERAL_TIM_CH4_PIN;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GENERAL_TIM_CH3_PORT, &GPIO_InitStructure);
 }
-
 
 ///*
 // * 注意：TIM_TimeBaseInitTypeDef结构体里面有5个成员，TIM6和TIM7的寄存器里面只有
@@ -79,11 +58,6 @@ static void GENERAL_TIM_Mode_Config(void)
 	TIM_TimeBaseInit(GENERAL_TIM, &TIM_TimeBaseStructure);
 
 	/*--------------------输出比较结构体初始化-------------------*/	
-	// 占空比配置
-	uint16_t CCR1_Val = 18;
-	uint16_t CCR2_Val = 4;
-	uint16_t CCR3_Val = 3;
-	uint16_t CCR4_Val = 2;
 	
 	TIM_OCInitTypeDef  TIM_OCInitStructure;
 	// 配置为PWM模式1
@@ -94,24 +68,9 @@ static void GENERAL_TIM_Mode_Config(void)
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 	
 	// 输出比较通道 1
-	TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+	TIM_OCInitStructure.TIM_Pulse = 0;
 	TIM_OC1Init(GENERAL_TIM, &TIM_OCInitStructure);
 	TIM_OC1PreloadConfig(GENERAL_TIM, TIM_OCPreload_Enable);
-	
-	// 输出比较通道 2
-	TIM_OCInitStructure.TIM_Pulse = CCR2_Val;
-	TIM_OC2Init(GENERAL_TIM, &TIM_OCInitStructure);
-	TIM_OC2PreloadConfig(GENERAL_TIM, TIM_OCPreload_Enable);
-	
-	// 输出比较通道 3
-	TIM_OCInitStructure.TIM_Pulse = CCR3_Val;
-	TIM_OC3Init(GENERAL_TIM, &TIM_OCInitStructure);
-	TIM_OC3PreloadConfig(GENERAL_TIM, TIM_OCPreload_Enable);
-	
-	// 输出比较通道 4
-	TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
-	TIM_OC4Init(GENERAL_TIM, &TIM_OCInitStructure);
-	TIM_OC4PreloadConfig(GENERAL_TIM, TIM_OCPreload_Enable);
 	
 	// 使能计数器
 	TIM_Cmd(GENERAL_TIM, ENABLE);
